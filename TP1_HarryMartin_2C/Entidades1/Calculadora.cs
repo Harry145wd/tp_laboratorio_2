8139e0b28f5d2ad99e174a8285cf10c508415e3b
Harry Martin <harrymartin145wd@hotmail.com>
2021-04-18 16:18:58 -0300

    added TP1 functional, without method's documentation

---
+++ TP1_HarryMartin_2C/Entidades1/Calculadora.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            char charOperador;
+            double resultado = 0;
+            if (!string.IsNullOrEmpty(operador))
+            {
+                char.TryParse(operador, out charOperador);
+                operador = ValidarOperador(charOperador);
+                switch (operador)
+                {
+                    case "+":
+                        {
+                            resultado = num1 + num2;
+                            break;
+                        }
+                    case "-":
+                        {
+                            resultado = num1 - num2;
+                            break;
+                        }
+                    case "*":
+                        {
+                            resultado = num1 * num2;
+                            break;
+                        }
+                    case "/":
+                        {
+                            resultado = num1 / num2;
+                            break;
+                        }
+                    default:
+                        {
+                            resultado = num1 + num2;
+                            break;
+                        }
+                }
+            }
+            return resultado;
+        }
+        private static string ValidarOperador(char operador)
+        {
+            string rtn= "+";
+            string auxArray = "+-*/";
+            foreach(char aux in auxArray)
+            {
+                if(operador == aux)
+                {
+                    rtn = $"{operador}";
+                    break;
+                }
+            }
+            return rtn;
+        }
+    }
+}
---
+++ TP1_HarryMartin_2C/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        //Methods
+        public static double Operar(string numero1,string numero2, string operador)
+        {
+           Numero num1 = new Numero(numero1.Trim());
+           Numero num2 = new Numero(numero2.Trim());
+           return Calculadora.Operar(num1, num2, operador);
+        }
+
+        public void Limpiar()
+        {
+            this.lblResultado.Text = string.Empty;
+            this.txtNumero1.Text = string.Empty;
+            this.txtNumero2.Text = string.Empty;
+        }
+
+        //Events
+
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = $"{Operar(this.txtNumero1.Text, this.txtNumero2.Text, this.cmbOperador.Text)}";
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = Numero.DecimalBinario(this.lblResultado.Text);
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = Numero.BinarioDecimal(this.lblResultado.Text); 
+        }
+
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Esta seguro que desea cerrar?", "Confirme si desea cerrar",
+                               MessageBoxButtons.YesNo,
+                               MessageBoxIcon.Exclamation,
+                               MessageBoxDefaultButton.Button2)==DialogResult.No)
+            {
+                e.Cancel = true;
+            } 
+        }
+
+       
+    }
+}
